03f2aafa35f00ab5fb718b183e246d177dfd38bd
Pratham24Precious <134937806+Pratham24Precious@users.noreply.github.com>
2023-05-29 19:07:29 +0530

    Update pr_12.rb

--- pr_12.rb
+++ pr_12.rb
@@ -1,3 +1,4 @@
+# 12. A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years. Ask user for their salary and year of service and print the net bonus amount.
 puts "enter your current salary"
 curr_salary = gets.to_i
 puts "enter your years of service"
