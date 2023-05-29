25a9c49a81e47b70c3edc8d62d9185d52325a41e
Pratham24Precious <134937806+Pratham24Precious@users.noreply.github.com>
2023-05-29 19:08:05 +0530

    Update pr_13.rb

--- pr_13.rb
+++ pr_13.rb
@@ -1,3 +1,10 @@
+# 13. A student will not be allowed to sit in exam if his/her attendence is less than 75%.
+#     # Take following input from user
+#     - Number of classes held
+#     - Number of classes attended.
+#     And print
+#     - percentage of class attended
+#     - Is student is allowed to sit in exam or not.
 puts "Enter number of classes held"
 held = gets.to_i
 puts "Enter number of classes attended."
